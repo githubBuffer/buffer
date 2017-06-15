5fb8e447be3b63c1f9105c987b9e6bc5d00a5a7c
githubBuffer <bufferemailbooking@gmail.com>
2017-06-15 13:40:45 +0300

    test project

---
+++ BufferProject/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Class of ${CLASS}.
+ *
+ * @author Siarhei Tuzhyk.
+ * @version 1.0
+ * @since 15.06.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Buffer project");
+    }
+}

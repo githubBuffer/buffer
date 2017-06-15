1e01239a8314cd76725916c0743991f525c27782
githubBuffer <bufferemailbooking@gmail.com>
2017-06-15 13:41:59 +0300

    Create Main.java

--- BufferProject/src/main/java/Main.java
+++ BufferProject/src/main/java/Main.java
@@ -7,6 +7,7 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Buffer project");
+        System.out.println("Buffer project!");
+        System.out.printl("Update project from github!");
     }
 }

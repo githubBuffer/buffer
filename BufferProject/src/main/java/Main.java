f04b261c09dbe18595df9572c9e0e7e413a4ec2f
githubBuffer <bufferemailbooking@gmail.com>
2017-06-16 10:24:38 +0300

    Update Main.java

--- BufferProject/src/main/java/Main.java
+++ BufferProject/src/main/java/Main.java
@@ -8,6 +8,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Buffer project!");
+        int x = 1;
+        for(int i=0;i<10;i++){
+            ++x=i++;
+        }
+        System.out.println(x);
         System.out.println("Update project from github!");
         System.out.println("Heyyyyyyooooooyyyyy");
         System.out.println("Repeate update project");
